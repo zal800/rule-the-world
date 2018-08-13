51853d8c0ecee1904047414dcce8f6c9a762472e
zal800 <42330504+zal800@users.noreply.github.com>
2018-08-12 23:13:08 -0400

    Add files via upload

---
+++ Practicing_Python.py
@@ -0,0 +1,435 @@
+
+# coding: utf-8
+
+# In[87]:
+
+
+None
+
+
+# In[ ]:
+
+
+#Arithmetic
+
+
+# In[2]:
+
+
+2*1*6*9*8*5*3*4*7*65
+
+
+# In[3]:
+
+
+(2*5) * (10+68)
+
+
+# In[4]:
+
+
+a = 10
+
+
+# In[5]:
+
+
+a
+
+
+# In[6]:
+
+
+a+a
+
+
+# In[8]:
+
+
+a=a +5
+
+
+# In[9]:
+
+
+a
+
+
+# In[11]:
+
+
+a = 10
+b = 20
+c = 30
+
+
+# In[12]:
+
+
+a = a + b
+
+
+# In[13]:
+
+
+a
+
+
+# In[14]:
+
+
+a = a
+
+
+# In[15]:
+
+
+a
+
+
+# In[16]:
+
+
+a = a - c
+
+
+# In[17]:
+
+
+a
+
+
+# In[18]:
+
+
+a + b + c
+
+
+# In[19]:
+
+
+a = a + b + c
+
+
+# In[20]:
+
+
+a
+
+
+# In[21]:
+
+
+b = b  + a + a + c + c
+
+
+# In[88]:
+
+
+b
+
+
+# In[89]:
+
+
+None
+
+
+# In[ ]:
+
+
+None
+
+
+# In[ ]:
+
+
+#Calculating income by defining variables
+
+
+# In[73]:
+
+
+my_income_daily = 200 #1 day
+my_income_weekly = 1000 #5 days
+my_income_bi_weekly = 2000 #10 days
+my_income_monthly = 4000 # 20 days
+
+
+# In[74]:
+
+
+#Note: There are 5 working days in a week
+
+#How much do I make in a day?
+#How much do I make in 3 days
+#how much do I make in a week?
+#How much do I make in 10 days?
+#How much do I make in 17 days?
+#How much do I make in 24 days?
+#How much do I make in 54 days?
+
+
+# In[75]:
+
+
+#How much do I make in a day?
+
+my_income_daily
+
+
+# In[76]:
+
+
+#How much do I make in 3 days
+
+my_income_daily * 3
+
+
+# In[77]:
+
+
+#how much do I make in a week? 
+
+my_income_weekly
+
+
+# In[80]:
+
+
+#How much do I make in 10 days?
+
+my_income_weekly * 2
+
+
+# In[82]:
+
+
+#How much do I make in 17 days?
+
+my_income_bi_weekly + my_income_weekly + my_income_daily *3
+
+
+# In[83]:
+
+
+#How much do I make in 24 days?
+
+my_income_monthly + my_income_daily * 4
+
+
+# In[84]:
+
+
+#How much do I make in 54 days?
+
+my_income_monthly * 2 + my_income_bi_weekly + my_income_daily * 4
+
+
+# In[111]:
+
+
+None
+
+
+# In[112]:
+
+
+None
+
+
+# In[113]:
+
+
+#Items and list of prices
+
+
+# In[98]:
+
+
+lb_apples = 3.99 #8 apples is a pound
+apples = 0.50
+lb_bananas = 3.99 #6 bananas is a pound
+bananas = 0.66
+lb_oranges = 3.99 #8 oranges is a pound
+oranges = 0.49
+lb_grapes = 4.99
+lb_kiwi = 3.49 #14 kiwis is a pound
+kiwi = 0.25
+
+
+# In[110]:
+
+
+#Answer all the question bellow.
+
+None
+#Question 1:
+#Customer wants 5 apples, a pound of bananas and a pound of grapes
+
+None
+
+#Question 2:
+#Customer wants 4 bananas, 7 kiwis, 10 apples and a pound of grapes
+
+#Question 3:
+#Which Calculating will be cheaper? going by single items or single -
+#and pounds and what is the difference?
+
+None
+#Question 4:
+#Customer wants 18 kiwis, 9 bananas, 25 apples and 30 oranges
+
+#Question 5:
+#Which Calculating will be cheaper? going by single items or single -
+#and pounds and what is the difference?
+
+
+# In[109]:
+
+
+#Customer wants 5 apples, a pound of bananas and a pound of grapes
+
+apples * 5 + lb_bananas + lb_grapes
+
+
+# In[115]:
+
+
+None
+
+
+# In[137]:
+
+
+#Question 2: Customer wants 4 bananas, 7 kiwis, 10 apples and a pound of grapes
+
+bananas * 4 + kiwi * 7 + lb_apples + apples * 2 + lb_grapes
+
+None 
+
+#How to calculate the answer if going by single items and pounds
+#0.66 * 4 = 2.64
+#0.25 * 7 = 1.75 
+#3.99 + 0.50 + 0.50 = 4.49
+#4.99 = 4.99 
+
+Total = 14.37
+
+None
+#If going by single items
+
+#4 bananas cost 2.64
+#7 kiwis cost 1.75
+#10 apples cost 5.00
+#grapes cost 4.99
+
+Total = 14.38
+
+
+# In[136]:
+
+
+bananas * 4 + kiwi * 7 + lb_apples + apples * 2 + lb_grapes
+
+
+# In[104]:
+
+
+bananas * 4 + kiwi * 7 + lb_apples + apples * 2 + lb_grapes
+
+
+# In[107]:
+
+
+#Question 3: Which Calculating will be cheaper? going by single items or single -
+#and pounds and what is the difference?
+
+None
+
+#Answer 3: Going by single items and pounds and the difference is 1 cent 
+
+
+# In[114]:
+
+
+None
+
+
+# In[138]:
+
+
+#Question 4:
+#Customer wants 18 kiwis, 9 bananas, 25 apples and 30 oranges
+
+lb_kiwi + kiwi * 4 + lb_bananas + bananas * 3 + lb_apples * 3 + apples + lb_oranges * 3 + oranges * 6
+
+#How to calculate the answer if going by single items and pounds
+None
+#Kiwis
+#3.49 = 3.49
+#0.25 * 4 = 1
+None
+#Bananas
+#3.99 = 3.99
+#0.66 * 3 = 1.98 
+None
+#Apples
+#3.99 * 3 = 11.97
+#0.50 = 0.50
+None
+#Oranges
+#3.99 * 3 =  11.97
+#0.49 * 6 = 2.94
+None
+#Total =  37.84
+
+None
+#If going by single items
+None
+#Kiwis
+#0.25 * 18 = 4.50
+None
+#Bananas
+#0.66 * 9 = 5.94
+None
+#Apples
+#0.50 * 25 = 12.50
+None
+#Oranges
+#0.49 * 30 = 14.7
+None
+#Total: 37.64
+
+None
+#Which price is cheaper?
+#Going by single items
+None
+
+#What is the difference?
+#37.84 - 37.64 = 0.20
+
+
+# In[131]:
+
+
+lb_kiwi + kiwi * 4 + lb_bananas + bananas * 3 + lb_apples * 3 + apples + lb_oranges * 3 + oranges * 6
+
+
+# In[139]:
+
+
+None
+
+
+# In[140]:
+
+
+None
+
+
+# In[141]:
+
+
+#Strings
+
